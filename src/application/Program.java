7f987ed6909e9a8aa07f3fa5d1de9aa71e55847e
EvertonConca <evertonconca@hotmail.com>
2025-06-14 11:00:58 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("ok");
+
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
 
 	}
 
